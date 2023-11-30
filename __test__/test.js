0c3000fee987e471a8958e149c0a99a111efc33c
Giovanni <giovanni.ricotta@ynov.com>
2023-11-30 14:57:38 +0100

    init

---
+++ __test__/test.js
@@ -0,0 +1,7 @@
+const test = from "../index.js"
+
+describe('first', () => { 
+    it('should ', () => {
+        const testd = new test
+    });
+ })
---
+++ index.js
@@ -0,0 +1,5 @@
+function test() {
+    return "toto"
+}
+
+export default test()
