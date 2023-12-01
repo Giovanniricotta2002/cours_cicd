416994ae688a3ba8ecba2807eb7b9d44673b8b16
Giovanni <giovanni.ricotta@ynov.com>
2023-12-01 11:45:25 +0100

    fix

--- func.js
+++ func.js
@@ -2,7 +2,7 @@ const Add = (...args) => args.reduce((a, b) => a + b);
 const Subtract = (...args) => args.reduce((a, b) => a - b);
 const Multiply = (...args) => args.reduce((a, b) => a * b);
 const Divide = (...args) => args.reduce((a, b) => a / b);
-const Test = () =>  "dd"
+const Test = "dd"
 
 const ComputeService = {
   Add, Subtract, Multiply, Divide, Test
