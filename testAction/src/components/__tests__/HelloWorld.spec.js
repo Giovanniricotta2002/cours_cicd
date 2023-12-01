6927dc67efb50ab9ab6d563d9277d128b0bd3fd5
Giovanni <giovanni.ricotta@ynov.com>
2023-12-01 13:19:12 +0100

    test

--- testAction/src/components/__tests__/HelloWorld.spec.js
+++ testAction/src/components/__tests__/HelloWorld.spec.js
@@ -2,10 +2,27 @@ import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import HelloWorld from '../HelloWorld.vue'
+import { useCounter } from '../test.vue'
+
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
+  it('renders properly', () => {
+    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitestd' } })
+    expect(wrapper.text()).toContain('Hello Vitest')
+  })
 })
+
+describe('counter', () => {
+  it('useCounter', () => {
+    const {count, increment} = useCounter()
+    expect(count.value).toBe(0)
+
+    increment()
+    expect(count.value).toBe(1)
+  });
+})
+
---
+++ testAction/src/components/test.vue
@@ -0,0 +1,13 @@
+<script>
+import { ref } from 'vue'
+
+export function useCounter() {
+    const count = ref(0)
+    const increment = () => count.value++
+    
+    return {
+        count,
+        increment
+    }
+}
+</script>
